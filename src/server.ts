9825a04ad73712ca8d35a3441f9a6cff311c4f18
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2025-05-10 15:37:27 -0300

    feat: add eslint

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ import { env } from './env'
 const PORT = env.PORT
 const HOST = '0.0.0.0'
 
-app.listen({
-  host: HOST,
-  port: PORT,
-}).then(() => {
-  console.log(`Server is running at http://${HOST}:${PORT}`)
-})
+app
+  .listen({
+    host: HOST,
+    port: PORT,
+  })
+  .then(() => {
+    console.log(`Server is running at http://${HOST}:${PORT}`)
+  })
