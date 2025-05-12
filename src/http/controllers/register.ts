7aa9223666a1cac213778d4c4c5b271a359d2f6c
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2025-05-11 22:28:37 -0300

    feat: add password hash and validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
-import { prisma } from '@/src/lib/prisma'
 import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
+import { prisma } from '@/src/lib/prisma'
+import { SALT_ROUNDS } from '@/src/settings'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerUserBodySchema = z.object({
@@ -11,11 +13,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerUserBodySchema.parse(request.body)
 
+  const userAlreadyExists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userAlreadyExists) {
+    return reply.status(409).send()
+  }
+
+  const password_hash = await hash(password, SALT_ROUNDS)
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
