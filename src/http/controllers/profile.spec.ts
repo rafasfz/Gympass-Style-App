240aaa6a191b9b87dfc432007b05c7cd0e7f1caf
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2025-05-16 17:57:17 -0300

    chore: create test for profile controller

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,44 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/src/app'
+
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    const user = {
+      name: 'John Lennon',
+      email: 'johnlennon@example.com',
+      password: '123123',
+    }
+
+    await request(app.server)
+      .post('/users')
+      .send({ ...user })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: user.email,
+      password: user.password,
+    })
+
+    const { token } = authResponse.body
+
+    const response = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      user: expect.objectContaining({
+        name: user.name,
+        email: user.email,
+      }),
+    })
+  })
+})
